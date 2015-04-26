67be5b667cfb5b8f194b864f6afef307554a7293
Ceren <akyildiz_ceren@hotmail.com>
2015-04-26 23:47:12 +0300

    RSA algoritması
    rsa algoritması ile encryption ve descryption programı

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace rsa2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(480, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(257, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "rsa";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Şifrelenmiş metin:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(237, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Çözümlenmiş Metin:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(240, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 404);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rsa2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var plainTextData = textBox1.Text;
+            var csp = new RSACryptoServiceProvider(2048);
+
+
+            var privKey = csp.ExportParameters(true);
+            var pubKey = csp.ExportParameters(false);
+
+
+            string pubKeyString;
+            {
+
+                var sw = new System.IO.StringWriter();
+                var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+
+                xs.Serialize(sw, pubKey);
+                pubKeyString = sw.ToString();
+            }
+
+
+            {
+
+                var sr = new System.IO.StringReader(pubKeyString);
+
+                var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+
+                pubKey = (RSAParameters)xs.Deserialize(sr);
+            }
+
+
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(pubKey);
+
+            var bytesPlainTextData = System.Text.Encoding.Unicode.GetBytes(plainTextData);
+
+
+            var bytesCypherText = csp.Encrypt(bytesPlainTextData, false);
+
+
+            var cypherText = Convert.ToBase64String(bytesCypherText);
+
+            label2.Text = cypherText;//RSA ya göre şifrelenmiş metin yazdırılıyor
+
+            //Bu adımdan sonra RSA ya göre şifrelenen veri ,orjinal haline döndürülüyor 
+            bytesCypherText = Convert.FromBase64String(cypherText);
+
+
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(privKey);
+
+            bytesPlainTextData = csp.Decrypt(bytesCypherText, false);
+
+
+            plainTextData = System.Text.Encoding.Unicode.GetString(bytesPlainTextData);
+            label4.Text = plainTextData;
+
+        }
+    }
+}
